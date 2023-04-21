44cddb354caa47a9513e826e79e8e9ee900024c5
JoaoVitorMirandaBandeira <joaovitormirandabandeira@gmail.com>
2023-04-21 16:09:14 -0300

    criando metodo de ler arquivo

--- Arquivo.java
+++ Arquivo.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Arquivo{
-    public static ListaDupla addJogadoresArquivo(ListaDupla lista){
-        
+    public static ListaDupla addJogadoresArquivo(ListaDupla ListaJogadores){
+        ListaDupla Lista = ListaJogadores
+        Scanner scanner = new Scanner(new File("jogadores.usu"));
+            while (scanner.hasNextLine()) {
+                String linha = scanner.nextLine();
+                String[] partes = linha.split(";");
+
+                int numero = Integer.parseInt(partes[0]);
+                String tipo = partes[1];
+                int pontuacao = Integer.parseInt(partes[2]);
+
+                Jogador jogador = new Jogador(numero, tipo, pontuacao);
+                Lista.add(novoJogador)
+            }
+            scanner.close();
+            return Lista;
     }
 }
